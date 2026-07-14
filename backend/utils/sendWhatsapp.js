17dd08c38089768a7c721a190e8b6d4b7637e08c
Lamininto <cloudstechtz@gmail.com>
2026-07-14 11:53:42 +0300

    Update sendWhatsapp.js

--- backend/utils/sendWhatsapp.js
+++ backend/utils/sendWhatsapp.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
-export const sendWtsp = async (recepient , message)=>{
-    try {
-        const response = await axios.post('http://api.textmebot.com/send.php' , {
-            recipient: recepient ,
-            apikey: process.env.WHATSAPP_API_KEY ,
-            text: message
-        })
-        return response
-    }
-    catch (err){
-         console.log(err)
-    }
+
+export const sendWtsp = async (recepient, message) => {
+  try {
+    const response = await axios.post('http://api.textmebot.com/send.php', {
+      recipient: recepient,
+      apikey: 'E2tf7r47y9au', // Your active TextMeBot API Key
+      text: message
+    })
+    return response
+  } catch (err) {
+    console.error("WhatsApp Send Error:", err)
+  }
 }
