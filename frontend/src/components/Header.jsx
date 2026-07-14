ac46bf4413b0187ba5e89be48966db73a1dbc45a
Lamininto <cloudstechtz@gmail.com>
2026-07-14 15:56:24 +0300

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = ({fixed = false})=>{
     return <header className={` px-4 md:px-[10%] py-4 text-white flex justify-between items-center ${fixed? 'fixed w-full bg-transparent' : 'sticky bg-primary'} top-0 z-20`}>
         <Link to='/' className='text-fourth text-2xl md:text-3xl uppercase'>
             efootball
-            <p className='text-sm'>By ourouimed</p>
+            <p className='text-sm'>By mbokatech</p>
         </Link>
         <nav className='hidden md:flex items-center gap-4 '>
             <ul className='bg-secondary rounded-full flex gap-2 justify-between items-center px-2 py-1'>
@@ -31,9 +31,9 @@ const Header = ({fixed = false})=>{
             </ul>
             <Link to='/dashboard' className='py-2 px-8 rounded-full block text-xl bg-third'>Admin</Link>
             <div className='hidden lg:flex items-center gap-1'>
-                <a className='flex items-center justify-center rounded-full w-[40px] h-[40px] bg-[#181717]' href='https://github.com/Ourouimed' target='_blank'><GitHub/></a>
-                <a className='flex items-center justify-center rounded-full w-[40px] h-[40px] bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600' href='https://instagram.com/Ourouimed' target='_blank'><Instagram/></a>
-                <a className='flex items-center justify-center rounded-full w-[40px] h-[40px] bg-[#25D366]' href='https://chat.whatsapp.com/BAo7vytVWKI05VpkhOXOd3' target='_blank'><WhatsApp/></a>
+                <a className='flex items-center justify-center rounded-full w-[40px] h-[40px] bg-[#181717]' href='https://github.com/clousdtechtz' target='_blank'><GitHub/></a>
+                <a className='flex items-center justify-center rounded-full w-[40px] h-[40px] bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600' href='https://instagram.com/Sultanseifu4' target='_blank'><Instagram/></a>
+                <a className='flex items-center justify-center rounded-full w-[40px] h-[40px] bg-[#25D366]' href='https://chat.whatsapp.com/HZOJv5WCltR3QYEjNwW53j?s=cl&p=a&ilr=4' target='_blank'><WhatsApp/></a>
             </div>
         </nav>
         <button className='bg-third p-2 rounded flex items-center justify-center cursor-pointer md:hidden' onClick={()=> {setMenuIsOpen(!MenuIsOpen)}}><Menu fontSize='medium'/></button>
