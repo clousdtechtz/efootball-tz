191b23a2d1f6526eea88b091c43778dc6dde17fe
Lamininto <cloudstechtz@gmail.com>
2026-07-14 15:09:37 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* Added basename so GitHub Pages routing works flawlessly! */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
     </BrowserRouter>
   </StrictMode>,
